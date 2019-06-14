ef9d07d340c53986a20d27c789ddc55117adda5c
Pinkolik <mrpinkolik@gmail.com>
2019-06-14 21:50:30 +0500

    implement recursion

--- VKHandShakes.py
+++ VKHandShakes.py
@@ -7,16 +7,30 @@ class VKHandShakes:
     def __init__(self, source_id: str, destination_id: str, max_depth: int = 5):
         self.source_id = source_id
         self.destination_id = destination_id
+        self.max_depth = max_depth
+        self.routes = []
         vk_session = vk_api.VkApi(token=VKHandShakes.service_token)
         self.vk = vk_session.get_api()
 
     def calculate_route(self):
         route = [self.source_id]
         depth = 0
-        self.calculate_route(route, depth)
+        self.search_in_friends(route.copy(), depth)
 
-    def calculate_route(self, route, depth):
+    def search_in_friends(self, route, depth):
+        depth += 1
+        if depth == self.max_depth:
+            return
         last_user_id = route[-1]
+        friends_ids = self.get_friends_user_ids(last_user_id)
+        if self.destination_id in friends_ids:
+            route.append(self.destination_id)
+            self.routes.append(route)
+            return
+        for user_id in friends_ids:
+            if user_id not in route:
+                new_route = route.copy().append(user_id)
+                self.search_in_friends(new_route, depth)
 
     def get_friends_user_ids(self, user_id) -> list:
         return self.vk.friends.get(user_id)['items']
